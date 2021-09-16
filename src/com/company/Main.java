c74f5350baa5a4edde2336d4fda2b24e859793dd
em <mironoff89@mail.ru>
2021-09-16 15:50:27 +0500

    from Soft Plus

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,36 @@
 package com.company;
 
-import org.jetbrains.annotations.Nullable;
-
 import java.util.ArrayList;
 
 public class Main {
-
     private static ArrayList<Integer> newPrice = new ArrayList<>();
-
+    
     public static void main(String[] args) {
-
-
         int[] price = {5,100,20,66,16};
-        decryptData(price,50,1,3);
+        decryptData(price,50,1,4);
         System.out.println(newPrice);
-
     }
 
-    public static @Nullable ArrayList<Integer> decryptData(@NonNull int[] price,
-                                                          @IntRange(from = 1) int discount,
-                                                          @IntRange(from = 0) int offset,
-                                                          @IntRange(from = 1) int readLength) {
-        for (int i = offset; i < readLength+1; i++) {
-            newPrice.add(price[i] = price[i]*discount/100);
+    public static ArrayList<Integer> decryptData(@NonNull int[] price,
+                                                 @IntRange(from = 1) int discount,
+                                                 @IntRange(from = 0) int offset,
+                                                 @IntRange(from = 1) int readLength) {
+        if (discount < 0 | offset < 0 | readLength < 0){
+            System.out.println("введите верные значения");
+            return null;
+        }
+        if(discount > 99){
+            System.out.println("скидка не может быть больше 99%");
+            return null;
+        }
+        int tempPrice;
+        for (int i = offset; i < readLength; i++) {
+            if (price[i] < 0){
+                System.out.println("в индексе " + readLength + " неверное значение =" +  " " + price[i]);
+                break;
+            }
+            tempPrice = (price[i]*discount/100);
+            newPrice.add((price[i] - tempPrice));
         }
         return newPrice;
     }
